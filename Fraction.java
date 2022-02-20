e6ebe531dbaad23b61e6087bd84b902f28e9c88e
angversh <91022755+angversh@users.noreply.github.com>
2022-02-20 23:40:48 +0300

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,34 +1,45 @@
 public class Fraction {
-    private int numer = 0;
-    private int denom = 0;
+    private int numerator = 0;
+    private int denominator = 0;
 
     //2 parameter constructor
     public Fraction(int numerator, int denominator) {
-        this.numer = numerator;
+        this.numerator = numerator;
         if (denominator == 0) { //check 4 zero
             throw new IllegalArgumentException(Integer.toString(denominator));
-        } else if(denominator < 0) {
-            this.numer *= -1;
-            this.denom = (-1) * denominator;
+        } else if (denominator < 0) {
+            this.numerator *= -1;
+            this.denominator = (-1) * denominator;
         } else {
-            this.denom = denominator;
+            this.denominator = denominator;
         }
     }
+
     //1 parameter constructor
     public Fraction(int numerator) {
         this(numerator, 1);
     }
 
-    public int getNumer(){
-        return this.numer;
+
+    public int getNumer() {
+        return this.numerator;
     }
 
-    public int getDenom(){
-        return this.denom;
+    public int getDenom() {
+        return this.denominator;
     }
 
-    // Greatest Common Divisor
-    private static int gcd(int num, int den){
+    public String toString() {
+        return this.numerator + "/" + this.denominator;
+    }
+
+    //result of numerator / denominator
+    public double toDouble() {
+        return (double) numerator / denominator; //change type to double so that integer division won't happen
+    }
+
+    //Greatest Common Divisor
+    private static int gcd(int num, int den) {
         if (den == 0) {
             return num;
         }
@@ -36,49 +47,53 @@ public class Fraction {
     }
 
     //Lowest Common Multiple
-    private static int lcm(int den1, int den2){
+    private static int lcm(int den1, int den2) {
         int numGCD = gcd(den1, den2);
-        return (den1 * den2)/ numGCD;
+        return (den1 * den2) / numGCD;
     }
 
-    public Fraction add(Fraction other){
-        int fracDenom = lcm(this.denom, other.denom);
-        int fracNumer = ((fracDenom/ this.denom) * this.numer) + ((fracDenom/ other.denom) * other.numer);
-        Fraction result = new Fraction(fracNumer, fracDenom);
+    public Fraction add(Fraction other) {
+        int fracDenominator = lcm(this.denominator, other.denominator);
+        int fracNumerator = ((fracDenominator / this.denominator) * this.numerator) + ((fracDenominator / other.denominator) * other.numerator);
+        Fraction result = new Fraction(fracNumerator, fracDenominator);
         return result;
     }
 
-    public Fraction subtract(Fraction other){
-        int fracDenom = lcm(this.denom, other.denom);
-        int fracNumer = ((fracDenom/ this.denom) * this.numer) - ((fracDenom/ other.denom) * other.numer);
-        Fraction result = new Fraction(fracNumer, fracDenom);
+
+    public Fraction subtract(Fraction other) {
+        int fracDenominator = lcm(this.denominator, other.denominator);
+        int fracNumerator = ((fracDenominator / this.denominator) * this.numerator) - ((fracDenominator / other.denominator) * other.numerator);
+        Fraction result = new Fraction(fracNumerator, fracDenominator);
         return result;
     }
 
-    public Fraction multiply(Fraction other){
-        int fracNumer =  this.numer * other.numer;
-        int fracDenom = this.denom * other.denom;
-        Fraction result = new Fraction(fracNumer, fracDenom);
+
+    public Fraction multiply(Fraction other) {
+        int fracNumerator = this.numerator * other.numerator;
+        int fracDenominator = this.denominator * other.denominator;
+        Fraction result = new Fraction(fracNumerator, fracDenominator);
         return result;
     }
 
-    public Fraction divide(Fraction other){
-        if(other.denom == 0){ //check 4 zero
-            throw new IllegalArgumentException(Integer.toString(denom));
+
+    public Fraction divide(Fraction other) {
+        if (other.denominator == 0) { //check 4 zero
+            throw new IllegalArgumentException(Integer.toString(denominator));
         }
-        int fracNumer =  this.numer * other.denom;
-        int fracDenom = this.denom * other.numer;
-        Fraction result = new Fraction(fracNumer, fracDenom);
+        int fracNumerator = this.numerator * other.denominator;
+        int fracDenominator = this.denominator * other.numerator;
+        Fraction result = new Fraction(fracNumerator, fracDenominator);
         return result;
     }
 
-    public void toLowestTerms(){
-        int numGCD = gcd(this.numer, this.denom);
-        this.numer /= numGCD;
-        this.denom /= numGCD;
-        if(this.denom < 0) {
-            this.numer *= -1;
-            this.denom *= -1;
+    //lower terms
+    public void toLowestTerms() {
+        int numGCD = gcd(this.numerator, this.denominator);
+        this.numerator /= numGCD;
+        this.denominator /= numGCD;
+        if (this.denominator < 0) {
+            this.numerator *= -1;
+            this.denominator *= -1;
         }
     }
 }
